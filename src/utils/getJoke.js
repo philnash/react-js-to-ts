6bea2d6a65a9fe328fe49fb21892046ce74b3068
Phil Nash <philnash@gmail.com>
2022-06-06 16:25:15 +1000

    Use superagent for API request.

--- src/utils/getJoke.js
+++ src/utils/getJoke.js
@@ -1,10 +1,15 @@
+import superagent from "superagent";
+
 const API_URL = "https://icanhazdadjoke.com/";
 
-async function getJoke() {
-  const jokeResponse = await fetch(API_URL, {
-    headers: { Accept: "application/json" },
-  }).then((res) => res.json());
-  return jokeResponse.joke;
-}
+const getJoke = async () => {
+  try {
+    const result = await superagent.get(API_URL).accept("json");
+    const dadJoke = result.body;
+    return dadJoke.joke;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export { getJoke };
